018fc38674e5fc7fe3be6eac2ec25ae8274e95a7
nosleN-belaC <101530598+nosleN-belaC@users.noreply.github.com>
2023-04-25 21:10:52 +1200

    Add files via upload

---
+++ SQL_CREATE_PROCEDURE.sql
@@ -0,0 +1,15 @@
+﻿
+CREATE PROCEDURE SelectAllCars
+AS
+SELECT * FROM dbo.Cars
+GO
+
+CREATE PROCEDURE SelectEngineType @Engine_ID int
+AS
+SELECT * FROM dbo.Cars WHERE Engine_ID = @Engine_ID AND Engine_ID = @Engine_ID
+GO
+
+CREATE PROCEDURE SelectDrivers @Hometown varchar(50)
+AS
+SELECT * FROM dbo.Drivers WHERE Hometown = @Hometown
+GO
